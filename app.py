7544b486cecaf20771dc100741e1a97bef78610e
dwner-dming <david.ming@downergroup.com>
2021-06-03 20:01:46 +1000

    add randomisation

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import date
+import random
+
+# seed rng based on date
+seed_val = date.today().toordinal()
+random.seed(seed_val)
+
+# shuffle names
+names = ["Nico", "Rui", "Kathleen", "Dave", "Michael"]
+random.shuffle(names)
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("hello.html", names=names, date_today=date.today())
