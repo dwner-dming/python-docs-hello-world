a95dea0f866282ed1a91848399e90c95b11dfd73
dwner-dming <david.ming@downergroup.com>
2021-06-04 09:24:05 +1000

    fix aus timezone

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
-from datetime import date
+from datetime import date, datetime
 import random
+import pytz
+
+# convert utc server time to aus date
+utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
+austime = utcnow.astimezone(pytz.timezone("Australia/NSW"))
+date_today = austime.date()
 
 # seed rng based on date
-seed_val = date.today().toordinal()
+seed_val = date_today.toordinal()
 random.seed(seed_val)
 
 # shuffle names
@@ -14,4 +20,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("hello.html", names=names, date_today=date.today())
+    return render_template("hello.html", names=names, date_today=date_today)
