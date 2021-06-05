2d3b82a22a16f6c67536f648312b2dc00a323c5c
dwner-dming <david.ming@downergroup.com>
2021-06-05 21:23:46 +1000

    fix update bug

--- app.py
+++ app.py
@@ -3,21 +3,27 @@ from datetime import date, datetime
 import random
 import pytz
 
-# convert utc server time to aus date
-utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
-austime = utcnow.astimezone(pytz.timezone("Australia/NSW"))
-date_today = austime.date()
 
-# seed rng based on date
-seed_val = date_today.toordinal()
-random.seed(seed_val)
+def process():
+    # convert utc server time to aus date
+    utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
+    austime = utcnow.astimezone(pytz.timezone("Australia/NSW"))
+    date_today = austime.date()
 
-# shuffle names
-names = ["Nico", "Rui", "Kathleen", "Dave"]
-random.shuffle(names)
+    # seed rng based on date
+    seed_val = date_today.toordinal()
+    random.seed(seed_val)
+
+    # shuffle names
+    names = ["Nico", "Rui", "Kathleen", "Dave"]
+    random.shuffle(names)
+
+    return (names, date_today)
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    names, date_today = process()
     return render_template("hello.html", names=names, date_today=date_today)
