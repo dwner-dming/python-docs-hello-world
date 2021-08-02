3bf83b735854aa95ddfda7cfe2a8889151957bbe
dwner-dming <david.ming@downergroup.com>
2021-08-03 09:15:18 +1000

    factor out name generation

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import pytz
 # from ipdb import set_trace
 
 
+def get_names_today(date_today: date) -> list:
+    """Returns a list of names conditional on whos is rostered for the day"""
+
+    names = ["Nico", "Rui", "Kathleen", "Dave"]
+    if date_today.weekday() in (3, 4):
+        names.append("Manpreet")
+
+    return names
+
+
 def process():
     # convert utc server time to aus date
     utcnow = datetime.utcnow().replace(tzinfo=pytz.utc)
@@ -16,9 +26,7 @@ def process():
     random.seed(seed_val)
 
     # shuffle names, add manpreet if on a Thurs or Fri
-    names = ["Nico", "Rui", "Kathleen", "Dave"]
-    if date_today.weekday() in (3, 4):
-        names.append("Manpreet")
+    names = get_names_today(date_today)
     random.shuffle(names)
 
     display_date = date_today.strftime("%A, %d %B %Y")
