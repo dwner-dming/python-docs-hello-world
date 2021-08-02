7c7ec0ad7e269c577190f87106692eb3f28c1ffd
dwner-dming <david.ming@downergroup.com>
2021-08-02 22:52:16 +1000

    update logic & styling

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from datetime import date, datetime
 import random
 import pytz
+# from ipdb import set_trace
 
 
 def process():
@@ -14,8 +15,10 @@ def process():
     seed_val = date_today.toordinal()
     random.seed(seed_val)
 
-    # shuffle names
-    names = ["Nico", "Rui", "Kathleen", "Dave", "Manpreet", "Michael"]
+    # shuffle names, add manpreet if on a Thurs or Fri
+    names = ["Nico", "Rui", "Kathleen", "Dave"]
+    if date_today.weekday() in (3, 4):
+        names.append("Manpreet")
     random.shuffle(names)
 
     display_date = date_today.strftime("%A, %d %B %Y")
@@ -28,4 +31,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     names, display_date = process()
-    return render_template("hello.html", names=names, disp_date=display_date)
+    return render_template("hello.html", names=names, n_names=len(names), disp_date=display_date)
